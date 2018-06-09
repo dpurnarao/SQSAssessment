f227e6d91271681e31d52592bea9a7db9a67ae13
ratna <ratna@Ratna.Home>
2018-06-09 14:36:49 +0100

    First

---
+++ Sample/src/main/java/SampleProject/Sample/App.java
@@ -0,0 +1,13 @@
+package SampleProject.Sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
